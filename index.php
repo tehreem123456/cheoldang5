3defa01d01c475298cecf792c61ff4d2b67611c1
tehreem123456 <137266904+tehreem123456@users.noreply.github.com>
2023-11-30 14:29:24 +0500

    add

---
+++ index.php
@@ -0,0 +1,819 @@
+<?php
+echo '
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- HTML TAGS IN HEADER,  EDIT EVERYTHING, INCLUSIVE REL CANONICAL -->
+
+<title>  "드럼통과 1톤용기 전문가 비결: 섬세한 유지보수 방법으로 오랜 수명과 안전 보장!"</title>
+<link rel="canonical" href="/index.php" />
+<link rel="shortcut icon" href="img/cheoldang-logo.png" />
+<meta charset="UTF-8">
+<meta name="description" content="드럼통과 1톤용기의 비밀 공개! 효과적인 유지보수 방법으로 내용물 보호하고 안전 책임지세요. 지금 클릭하여 전문가 팁 확인!">
+<meta name="keywords" content=드럼통과 1톤용기, 유지보수 팁, 1톤용기 관리, 안전한 드럼통과, 효과적인 유지보수>
+<meta name="author" content="cheoldang">
+
+
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css">
+<link rel="stylesheet" href="https://netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css">
+
+  <!-- STYLESHEET, REMEMBER TO EDIT BACKGROUND IMAGE URLS -->
+
+  <style>
+    @import url(\'https://fonts.googleapis.com/css2?family=Domine:wght@400;700&family=Open+Sans:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap\');
+
+    * {
+      font-family: \'Open Sans\', sans-serif;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+    }
+
+    body {
+      font-family: \'Montserrat\', sans-serif;
+    }
+
+    a {
+      text-decoration: none;
+    }
+
+    .top-sec a {
+      text-shadow: 1px 1px #000000;
+    }
+
+    .boxes ul {
+      float: unset !important;
+      list-style: disc;
+      margin-left: 30px;
+    }
+
+    .boxes ul li {
+      margin-bottom: 5px;
+    }
+    .boxes ul li a {
+      color: #000000;
+    }
+
+
+    .container {
+      padding: 10px 70px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
+
+
+    .logo-box a {
+      outline: none;
+      display: block;
+    }
+
+    .logo-box img {
+      display: block;
+      max-width: 225px;
+    }
+
+    nav {
+      overflow: hidden;
+    }
+
+    ul {
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      float: right;
+    }
+
+    nav li {
+      display: inline-block;
+      margin-left: 25px;
+      height: 70px;
+      line-height: 70px;
+      transition: .5s linear;
+    }
+
+    nav a {
+      text-decoration: none;
+      display: block;
+      position: relative;
+      color: #868686;
+      text-transform: uppercase;
+    }
+
+    nav a:after {
+      content: "";
+      width: 0;
+      height: 2px;
+      position: absolute;
+      left: 0;
+      bottom: 15px;
+      background: #868686;
+      transition: width .5s linear;
+    }
+
+    nav a:hover:after {
+      width: 100%;
+    }
+
+
+    .sec-k {
+      text-align: center;
+    }
+
+    @media screen and (max-width: 660px) {
+      header {
+        text-align: center;
+      }
+
+      .logo-box {
+        float: none;
+        display: inline-block;
+        margin: 0 0 16px 0;
+      }
+
+      ul {
+        float: none;
+      }
+
+      nav li:first-of-type {
+        margin-left: 0;
+      }
+    }
+
+    @media screen and (max-width: 550px) {
+      nav {
+        overflow: visible;
+      }
+
+      nav li {
+        display: block;
+        margin: 0;
+        height: 40px;
+        line-height: 40px;
+      }
+
+      nav li:hover {
+        background: rgba(0, 0, 0, .1);
+      }
+
+      nav a:after {
+        content: none;
+      }
+    }
+
+
+
+    .top-sec {
+      background-image: linear-gradient(to bottom, #1010109b, #1010109b), url(img/main.jpg);
+      background-position: center center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding: 130px 70px;
+    }
+
+    .top-sec p,
+    .top-sec h1,
+    .top-sec a {
+      text-align: center;
+      color: #fff;
+    }
+    .top-sec h1 {
+      font-size: 50px;
+      margin-bottom: 30px;
+    }
+    .top-sec p {
+      margin-bottom: 10px;
+    }
+
+    .wrapper-content {
+      max-width: 1300px;
+      margin: 0 auto;
+    }
+
+    .center {
+      text-align: center;
+    }
+
+    .icon-wrap {
+      display: flex;
+    }
+
+    .icon-box {
+      text-align: center;
+      width: 50%;
+      margin-top: 3rem;
+    }
+
+    .price {
+      color: #ff7e0c;
+      font-size: 42px;
+    }
+
+    .cta-btn {
+      letter-spacing: 1.5px;
+      fill: #fff;
+      color: #fff;
+      background-color: #ff7e0c;
+      border-radius: 36px 36px 36px 36px;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5);
+      padding: 10px 20px 10px 20px;
+      position: relative;
+      top: 1rem;
+      text-decoration: none;
+    }
+
+    .image-wrap {
+      margin-top: 0;
+      padding: 20px 70px;
+      background: #F7F8FA;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5) inset;
+      transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+    }
+
+    .image-wrap2 {
+      margin-top: 5rem;
+      margin-bottom: 5rem;
+      padding-bottom: 5rem;
+      background: #F7F8FA;
+      box-shadow: 0 0 10px 0 rgba(0, 0, 0, .5) inset;
+      transition: background 0.3s, border 0.3s, border-radius 0.3s, box-shadow 0.3s;
+    }
+
+    .boxes {
+      display: flex;
+      padding-top: 2rem;
+      padding-bottom: 0rem;
+    }
+
+
+    .box {
+      width: 50%;
+    }
+    .box h2 {
+      margin-bottom: 10px;
+    }
+
+    .box:nth-of-type(1) {
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+    }
+
+    .imagebox {
+      max-width: 100%;
+      width: 90%;
+      padding-left: 35px;
+      padding-top: 50px;
+      box-shadow: 1px 1px 11px -1px #ccc;
+      border-radius: 5px;
+      margin: 20px 0;
+    }
+
+    .imagebox:nth-of-type(1) {
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position: 50% 50%;
+    }
+
+    .text {
+      text-align: left;
+      padding: 0px 25px;
+      word-break: break-word;
+    }
+
+
+    .grid-container {
+      display: grid;
+      grid-template-columns: auto auto;
+      padding: 10px;
+    }
+
+
+    .spacing {
+      padding-bottom: 3rem;
+      padding-left: 3rem;
+    }
+
+    footer .row {
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+    }
+    .column {
+      /* float: left; */
+      width: 25%;
+      padding: 2rem 0rem;
+    }
+    footer .column p {
+      margin-bottom: 5px;
+    }
+
+    /* Clear floats after the columns */
+    .row:after {
+      content: "";
+      display: table;
+      clear: both;
+    }
+
+    footer {
+      background: #3A3A3A;
+      color: #fff;
+      font-size: 14px;
+      padding: 20px 70px;
+    }
+
+    footer a {
+      color: #fff;
+      text-decoration: none;
+      display: block;
+      margin-bottom: 5px;
+    }
+
+    .sub {
+      font-size: 18px;
+      font-weight: 600;
+      margin-bottom: 15px !important;
+    }
+
+    /* Wrapper */
+    .icon-button {
+      background-color: white;
+      border-radius: 2.6rem;
+      cursor: pointer;
+      display: inline-block;
+      font-size: 1.3rem;
+      height: 2.6rem;
+      line-height: 2.6rem;
+      margin: 0 5px;
+      position: relative;
+      text-align: center;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+      width: 2.6rem;
+    }
+
+    /* Circle */
+    .icon-button span {
+      border-radius: 0;
+      display: block;
+      height: 0;
+      left: 50%;
+      margin: 0;
+      position: absolute;
+      top: 50%;
+      -webkit-transition: all 0.3s;
+      -moz-transition: all 0.3s;
+      -o-transition: all 0.3s;
+      transition: all 0.3s;
+      width: 0;
+    }
+
+    .icon-button:hover span {
+      width: 2.6rem;
+      height: 2.6rem;
+      border-radius: 2.6rem;
+      margin: -1.3rem;
+    }
+
+    /* Icons */
+    .icon-button i {
+      background: none;
+      color: white;
+      height: 2.6rem;
+      left: 0;
+      line-height: 2.6rem;
+      position: absolute;
+      top: 0;
+      -webkit-transition: all 0.3s;
+      -moz-transition: all 0.3s;
+      -o-transition: all 0.3s;
+      transition: all 0.3s;
+      width: 2.6rem;
+      z-index: 10;
+    }
+
+
+    .twitter span {
+      background-color: #4099ff;
+    }
+
+    .facebook span {
+      background-color: #3B5998;
+    }
+
+    .google-plus span {
+      background-color: #789cc1;
+    }
+
+    .tumblr span {
+      background-color: #34526f;
+    }
+
+    .instagram span {
+      background-color: #517fa4;
+    }
+
+    .youtube span {
+      background-color: #bb0000;
+    }
+
+    .pinterest span {
+      background-color: #0A66C2;
+    }
+
+    .icon-button .icon-twitter {
+      color: #4099ff;
+    }
+
+    .icon-button .icon-facebook {
+      color: #3B5998;
+    }
+    .icon-button .fa-pinterest {
+      color: #db5a3c;
+    }
+
+    .icon-button .fa-tumblr {
+      color: #34526f;
+    }
+
+    .icon-button .icon-google-plus {
+      color: #db5a3c;
+    }
+
+    .icon-button .fa-instagram {
+      color: #517fa4;
+    }
+
+    .icon-button .fa-youtube {
+      color: #bb0000;
+    }
+
+    .icon-button .fa-linkedin {
+      color: #0A66C2;
+    }
+
+
+
+
+    .icon-button:hover .icon-twitter,
+    .icon-button:hover .icon-facebook,
+    .icon-button:hover .icon-google-plus,
+    .icon-button:hover .fa-tumblr,
+    .icon-button:hover .fa-instagram,
+    .icon-button:hover .fa-youtube,
+    .icon-button:hover .fa-linkedin,
+    .icon-button:hover .fa-pinterest {
+      color: white;
+    }
+
+    @media all and (max-width: 680px) {
+      .icon-button {
+        border-radius: 1.6rem;
+        font-size: 0.8rem;
+        height: 1.6rem;
+        line-height: 1.6rem;
+        width: 1.6rem;
+      }
+
+      .icon-button:hover span {
+        width: 1.6rem;
+        height: 1.6rem;
+        border-radius: 1.6rem;
+        margin: -0.8rem;
+      }
+
+      /* Icons */
+      .icon-button i {
+        height: 1.6rem;
+        line-height: 1.6rem;
+        width: 1.6rem;
+      }
+
+      .pinterest {
+        display: none;
+      }
+
+    }
+
+
+
+    @media all and (max-width: 1130px) {
+
+      .top-sec,
+      .icon-wrap,
+      .boxes {
+        display: block;
+      }
+
+      iframe {
+        width: 95%;
+      }
+
+      .text {
+        padding: 0px;
+      }
+
+      .box,
+      .icon-box {
+        width: 100%;
+        text-align: center;
+        display: block;
+      }
+
+      .wrapper-content {
+        padding: 50px;
+      }
+
+      .top-sec {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 50px;
+        height: 350px;
+        font-size: 26px !important;
+      }
+
+      .spacing {
+        padding-bottom: 3rem;
+        padding-top: 1rem;
+        padding-left: 0rem;
+      }
+
+      .icon-box {
+        margin-bottom: 7rem;
+      }
+
+      .top-sec a {
+        font-size: 24px !important;
+      }
+
+      .column {
+        width: 50%;
+      }
+
+      .link a {
+        font-size: 2.4rem;
+      }
+    }
+  </style> 
+
+</head>
+    
+<body>
+<header>
+
+
+ <div class="container">
+  
+  <!-- INSERT LOGO URL AND /how-to-use-twitter-moments-for-local-seo.html -->
+  
+    <div class="logo-box">
+      <a href="/index.php">
+        <img alt="cheoldang-logo" src="img/cheoldang-logo.png" id="cheoldang-logo" data-test-source="cheoldang-logo" data-test-pro-id="cheoldang-logo" data-test-pro-name="cheoldang-logo" data-test="cheoldang-logo" title="cheoldang-logo">
+      </a>
+    </div>
+    <nav>
+    <ul>
+    
+    <!-- NAVIGATION MENU INSERT ALL GOOGLE PROPERTIES AND UMBRELLA PROPERTIES -->
+    
+      <li><a href="https://www.cheoldang.co.kr/">ibc탱크</a></li>
+   </ul>
+  </nav>
+  </div>
+</header>
+
+
+<section class="top-sec">
+
+<div>
+
+<!-- INSERT MAIN KEYWORDS IN THE H1 TAG, AND INSERT NAP INFO BELOW -->
+
+<h1><span id="  "드럼통과 1톤용기 전문가 비결: 섬세한 유지보수 방법으로 오랜 수명과 안전 보장!""><a href="https://www.cheoldang.co.kr/">  "드럼통과 1톤용기 전문가 비결: 섬세한 유지보수 방법으로 오랜 수명과 안전 보장!"</a></span></h1>
+<p><a href="https://www.cheoldang.co.kr/" target="_none">  "드럼통과 1톤용기 전문가 비결: 섬세한 유지보수 방법으로 오랜 수명과 안전 보장!"</a></p>
+</div>
+    
+</section>
+
+<!-- INSERT CONTENT AND LINKS TO MONEYSITE -->
+
+
+<section class="image-wrap">
+    <div class="wrapper-content boxes">
+    <div class="box">     
+    
+
+  <img class="imagebox" alt="액체-벌크-용기" src="img/액체-벌크-용기.jpg" id="액체-벌크-용기" data-test-source="액체-벌크-용기" data-test-pro-id="액체-벌크-용기" data-test-pro-name="액체-벌크-용기" data-test="액체-벌크-용기" title="액체-벌크-용기">
+	
+    <img class="imagebox" alt="석유-저장-탱크-산업-단지" src="img/석유-저장-탱크-산업-단지.jpg" id="석유-저장-탱크-산업-단지" data-test-source="석유-저장-탱크-산업-단지" data-test-pro-id="석유-저장-탱크-산업-단지" data-test-pro-name="석유-저장-탱크-산업-단지" data-test="석유-저장-탱크-산업-단지" title="석유-저장-탱크-산업-단지">
+
+    <img class="imagebox" alt="원유-창고" src="img/원유-창고
+.jpg" id="원유-창고" data-test-source="원유-창고" data-test-pro-id="원유-창고" data-test-pro-name="근육-풀어주기와-유연
+성-향상" data-test="원유-창고" title="원유-창고">
+
+메이크업-아티스트가-여자에게-화장을-해준다
+
+얼굴에-파우더를-바르는-여자
+
+
+
+    </div>
+    <div class="box spacing">
+    
+<!-- INSERT MAIN CONTENT, AND LINKS -->
+    
+<ol>
+ 	<li><strong>소개</strong>
+<ul>
+ 	<li>드럼통과 1톤용기와 그 중요성 간략 소개.</li>
+</ul>
+</li>
+ 	<li><strong>유지보수의 중요성 이해하기</strong>
+<ul>
+ 	<li>유지보수가 1톤 드럼 통과의 장수와 효율성을 보장하는 데 어떻게 중요한지 강조.</li>
+</ul>
+</li>
+ 	<li><strong>드럼통과 1톤용기 유지보수의 공통된 어려움</strong>
+<ul>
+ 	<li>이 용기를 유지하는 데 사용자가 직면할 수 있는 일반적인 문제에 대한 논의.</li>
+</ul>
+</li>
+ 	<li><strong>정기적인 청소 절차</strong>
+<ul>
+ 	<li>1톤 드럼 통과를 정기적으로 청소하고 유지하는 단계별 가이드.</li>
+</ul>
+</li>
+ 	<li><strong>마모 및 파손 점검</strong>
+<ul>
+ 	<li>마모와 파손의 징후를 확인하고 잠재적인 문제를 식별하는 방법에 대한 중요성 강조.</li>
+</ul>
+</li>
+ 	<li><strong>안전 기준 준수</strong>
+<ul>
+ 	<li>1톤 드럼 통과의 유지보수와 관련된 안전 조치와 표준에 대한 설명.</li>
+</ul>
+</li>
+ 	<li><strong>적절한 청소제 선택</strong>
+<ul>
+ 	<li>1톤 드럼 통과의 유지보수에 적합한 청소제 추천.</li>
+</ul>
+</li>
+ 	<li><strong>계절별 유지보수 꿀팁</strong>
+<ul>
+ 	<li>계절에 따라 특정한 유지보수 팁에 대한 논의.</li>
+</ul>
+</li>
+ 	<li><strong>DIY 대 전문가 서비스</strong>
+<ul>
+ 	<li>도-it-yourself 유지보수와 전문가 서비스를 비교하여 장단점 논의.</li>
+</ul>
+</li>
+ 	<li><strong>비용 효율적인 유지보수 전략</strong>
+<ul>
+ 	<li>1톤 드럼 통과를 저렴하게 유지하는 방법에 대한 팁.</li>
+</ul>
+</li>
+ 	<li><strong>사례 연구: 성공적인 유지보수 사례</strong>
+<ul>
+ 	<li>1톤 드럼 통과를 효과적으로 유지한 기업이나 개인의 실제 사례.</li>
+</ul>
+</li>
+ 	<li><strong>적시 수리의 중요성</strong>
+<ul>
+ 	<li>작은 수리부터 큰 문제를 방지하기 위해 적시에 수리하는 중요성 강조.</li>
+</ul>
+</li>
+ 	<li><strong>적절한 유지보수의 환경 영향</strong>
+<ul>
+ 	<li>적절한 유지보수가 환경 지속 가능성에 어떻게 기여하는지 논의.</li>
+</ul>
+</li>
+ 	<li><strong>유지보수의 미래 동향</strong>
+<ul>
+ 	<li>1톤 드럼 통과의 유지보수에서 나타나는 혁신적인 동향에 간단히 언급.</li>
+</ul>
+</li>
+ 	<li><strong>결론</strong>
+<ul>
+ 	<li>주요 포인트 요약 및 독자에게 자신의 드럼통과 1톤용기 유지보수를 우선시하도록 권장.</li>
+</ul>
+</li>
+</ol>
+
+<hr />
+
+<strong>자주 묻는 질문:</strong>
+<ol>
+ 	<li><strong>Q: 1톤 드럼 통과를 얼마나 자주 청소해야 하나요?</strong>
+<ul>
+ 	<li>A: 사용량 및 환경적인 요소에 따라 다르지만 월간 청소가 좋은 시작점입니다.</li>
+</ul>
+</li>
+ 	<li><strong>Q: 유지보수를 위해 어떤 청소제를 사용해야 하나요?</strong>
+<ul>
+ 	<li>A: 제조사가 권장하는 청소제나 산업용 용기에 적합한 청소제를 사용하는 것이 좋습니다.</li>
+</ul>
+</li>
+ 	<li><strong>Q: 전문가 유지보수 서비스는 비싸요가요?</strong>
+<ul>
+ 	<li>A: 비용은 다양하지만 전문가 서비스에 투자하면 주요 문제를 방지해 장기적으로 비용을 절약할 수 있습니다.</li>
+</ul>
+</li>
+ 	<li><strong>Q: 마모 및 파손의 징후는 무엇인가요?</strong>
+<ul>
+ 	<li>A: 녹, 찍힘 또는 구조적인 변화를 확인하십시오. 사용 중 이상한 소리가 나면 잠재적인 문제가 있을 수 있습니다.</li>
+</ul>
+</li>
+ 	<li><strong>Q: 유지보수가 환경 지속 가능성에 어떻게 기여하나요?</strong>
+<ul>
+ 	<li>A: 적절한 유지보수로 자주 교체할 필요가 없어져 폐기물과 환경에 미치는 영향을 최소화합니다.</li>
+</ul>
+</li>
+</ol>   
+  
+    </div>
+    </div>
+</section >
+
+    
+
+    
+<!-- INSERT GOOGLE MAPS EMBED, IF COMPANY HAS GMB PROFILE, INCLUDE CID IN THE URL -->
+
+
+ 
+
+
+<!-- INSERT SECONDARY KEYWORD ANCHOR TEXT LINKS TO GOOGLE MAPS INCLUDING CID IN THE URL. IF NO GOOGLE MAPS PROFILE EXISTS, THEN LINK TO GOOGLE NEW SITE -->
+    
+
+
+
+
+
+
+    
+<footer>
+    <div class="wrapper-content">
+        <div class="row">
+  <div class="column"><img src="img/cheoldang-logo.png" alt="cheoldang-logo" style="max-width: 225px;">
+            
+     <!-- INSERT COMPANY INFO -->
+<br>
+CHEOLDANG.CO.,LTD경상남도 양산시 산막공단 북2길 22<br>
+Tel. 055-362-1703 | Mobile. 010-7354-1703 | E-mail. kdi1703@naver.com<br>
+
+            
+       </div>
+       
+       <!-- INSERT LINKS TO SERVICES IN MAIN MONEY SITE -->
+       
+<div class="column"><p class="sub">카테고리</p>
+<a href="https://www.cheoldang.co.kr/loadPage.do?page=company">회사소개 | 철당(주)</a>
+<a href="https://www.cheoldang.co.kr/loadPage.do?page=producing">생산능력-IBC탱크 전문-철당(주)</a>
+<a href="https://www.cheoldang.co.kr/loadPage.do?page=ibc_about">IBC탱크란-철당(주)-제품소개</a>
+<a href="https://www.cheoldang.co.kr/loadPage.do?page=ibc_classic">반제품IBC탱크-철당(주)</a>
+<a href="https://www.cheoldang.co.kr/loadPage.do?page=drum_steel">철드럼-철당(주)</a>
+<a href="https://www.cheoldang.co.kr/boardPage.do?board_gb=consumer">고객문의게시판 | IBC탱크 전문 - 철당</a>
+<a href="https://www.cheoldang.co.kr/boardPage.do?board_gb=caution">공지사항 | 철당(주)</a>
+<a href="https://smartstore.naver.com/cheoldang1703">철당</a>
+<a href="https://blog.naver.com/kdi1703">IBC탱크,(PE,공드럼,오픈)드럼 재생전문업체 철당 : 네이버 블로그</a>
+<a href="https://www.instagram.com/cheoldang/">철당 (@cheoldang) • Instagram photos and videos</a>
+
+
+
+</div>
+
+<!-- INSERT LINKS TO BLOG POSTS -->
+<div>
+<H2>당신이 관심을 가질 만한 기사</H2>
+<br>
+In Progress
+
+
+<p>
+<br>
+</p>
+    </div>
+</div>
+    </div>
+    
+    
+    </footer>
+
+
+<!-- Simple cloud HTML template created by Jesper Nissen SEO at https://jespernissen.com -->
+
+</body>
+
+</html>
+';
+?>
